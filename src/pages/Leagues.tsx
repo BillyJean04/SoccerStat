d9afd7809d55f410740dae99a223ec1b02f96e24
BillyJean <bozhozda@gmail.com>
2024-03-24 18:01:06 +0300

    feat: remove fetching competitions

--- src/pages/Leagues.tsx
+++ src/pages/Leagues.tsx
@@ -1,19 +1,5 @@
-import { useEffect, useState } from "react";
-
 import Container from "@/components/Container";
 
-async function fetchCompetitions() {
-    return fetch("https://api.football-data.org/v4/competitions").then((res) => res.json());
-}
-
 export default function LeaguesPage() {
-    const [teams, setTeams] = useState();
-
-    useEffect(() => {
-        fetchCompetitions().then((res) => setTeams(res.competitions));
-    }, []);
-
-    console.log(teams);
-
     return <Container>Leagues</Container>;
 }
