cdae328f08b8a76cc547ef0fbf4cdd82e2ae66e0
BillyJean <bozhozda@gmail.com>
2024-03-31 17:18:43 +0300

    chore: change base name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/soccer-stat/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
